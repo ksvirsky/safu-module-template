e68f0c7c709c0ad4d3a8dbe309d1f366b2ee0dcb
Kanstantsin Svirski <kanstantsin.svirski@altoros.com>
2024-03-15 14:28:34 +0100

    - resolving hydration errors;

--- components/page-content.tsx
+++ components/page-content.tsx
@@ -65,6 +65,7 @@ export function PageContent(props: { items: Array<PageItems | PageItemsTabsTabsI
 
         return <div className="mb-4 mx-6" key={index + item?.__typename} data-tina-field={tinaField(component, "url")}>
           <iframe
+            suppressHydrationWarning
             allow="solana; ethereum"
             className="w-full"
             height={"800px"}
@@ -77,6 +78,7 @@ export function PageContent(props: { items: Array<PageItems | PageItemsTabsTabsI
 
         return <div className="mb-4 mx-6" key={index + item?.__typename} data-tina-field={tinaField(component, "url")}>
           <iframe
+            suppressHydrationWarning
             allow="solana; ethereum"
             className="w-full"
             height={"800px"}
@@ -90,7 +92,9 @@ export function PageContent(props: { items: Array<PageItems | PageItemsTabsTabsI
         const widgetProps = parseProps(component?.widgetProps);
 
         return <div className="mb-4 mx-6" key={index + item?.__typename} data-tina-field={tinaField(component, "widgetProps")}>
-          <div className="rm-widget"
+          <div
+            suppressHydrationWarning
+            className="rm-widget"
           {...widgetProps}
           />
         </div>;
