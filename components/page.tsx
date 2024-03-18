6a72f2063f1419b9917e925813673dfe2a852fbb
Kanstantsin Svirski <kanstantsin.svirski@altoros.com>
2024-03-18 07:50:09 +0100

    - fixed hydration errors;

--- components/page.tsx
+++ components/page.tsx
@@ -17,7 +17,7 @@ export function Page(props: {
   const homeData = (!props.homeData || props.data === props.homeData) ? data : props.homeData;
 
   return (
-    <main suppressHydrationWarning className="flex min-h-screen flex-col items-center max-w-screen-xl mx-auto">
+    <main className="flex min-h-screen flex-col items-center max-w-screen-xl mx-auto">
       <PageHeader pageData={homeData}>
         {homeData?.page.menuItems && <Menu items={homeData.page.menuItems} />}
       </PageHeader>
